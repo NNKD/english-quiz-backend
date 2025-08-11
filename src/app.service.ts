537acfe9272535110018f05a9dc465d2bcf2f8ad
NNKD <duynguyenngockhanh@gmail.com>
2025-08-11 14:47:33 +0700

    chore: setup MongoDB connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import env from './config/env';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World! ${env().DB_HOST}, ${env().DB_NAME}`;
+    return `Hello World!`;
   }
 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,5 @@
 import * as process from 'node:process';
 
 export default () => ({
-  DB_NAME: process.env.DB_NAME,
-  DB_HOST: process.env.DB_HOST,
+  DB_URL: process.env.DB_URL,
 });
