6f0876beda68ef099c4d80851040955eb6a6d54f
NNKD <duynguyenngockhanh@gmail.com>
2025-08-12 14:37:52 +0700

    fix(register): handle existing user in send mail and fix CORS issue

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { CustomValidationPipe } from './validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new CustomValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
