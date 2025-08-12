dbe8b0f4daa1f6a99f6bc861bd185a5d048ae246
NNKD <duynguyenngockhanh@gmail.com>
2025-08-12 19:40:03 +0700

    feat(login): add jwt, bcrypt hash password and login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import type { RequestWithUser } from './auth.interface';
+import { SignInDto } from '../dtos/sign-in.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: RequestWithUser) {
+    return req.user;
+  }
+}
--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,5 @@ export default () => ({
   DB_URL: process.env.DB_URL,
   MAIL_USER: process.env.MAIL_USER,
   MAIL_PASS: process.env.MAIL_PASS,
+  JWT_SECRET: process.env.JWT_SECRET,
 });
